f8c0f7fc59b6d93287c97603ce6695e324db82e5
Alex Huang <109326746+alexziao05@users.noreply.github.com>
2024-12-09 01:03:10 -0800

    Implemented Prim's Algo

--- graph.cpp
+++ graph.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
+#include <cstring> 
 using namespace std; // Now we can use standard library names without the `std::` prefix
 
 // Global Variables
@@ -92,19 +93,48 @@ int Graph::primMST(vector<int>& mstStart, vector<int>& mstEnd) {
     // Placeholder for visited nodes and minimum weight initialization
     cout << "TODO: Implement Prim's algorithm here.\n";
 
-    // Definition of MST 
-
     // Steps:
     // 1. Initialize visited array and minWeight array
+    int visited_array[numNodes]; 
+    memset(visited_array, false, sizeof(visited_array)); // Initialize with false
     // 2. Start from node 1
+    visited_array[0] = true; // Start with the 0th vertex
     // 3. Use a loop to find the minimum weight edge at each step
+    // MST Edges = Vertices - 1 
+    cout << "Edge : Weight\n"; 
+    int n_edges = 0; 
+    int totalCost = 0; 
+    while (n_edges < numNodes - 1) { // Make sure each vertex is hit at least once (Definition of MST)
+        int min = INT_MAX; // Make sure we choose the minimum weight 
+        int r=0, c=0; // R = row, C = column
+
+        for (int i = 0; i < numNodes; ++i) { //Traverse through the row to pick every vertex 
+            if (visited_array[i]) { // Check if it is inside visited array only when it is TRUE
+                for (int j = 0; j < numNodes; ++j) { //Traverse through columns 
+                    // Find the min value of a particular row 
+                    if (min > adjMatrix[i][j]) {
+                        if(!visited_array[j] && adjMatrix[i][j]) { // Make sure it is not visited and that it is not 0. We are not concerned with these values.
+                            min = adjMatrix[i][j]; 
+                            r = i; 
+                            c = j; 
+                        } 
+                    }
+                }
+
+            }
+        }
+        mstStart.push_back(r);
+        mstEnd.push_back(c);
+        visited_array[c] = true; 
+        n_edges++; 
+        totalCost += adjMatrix[r][c]; 
+    }
     // 4. Update MST edges (mstStart and mstEnd vectors)
     // 5. Return the total cost of the MST
 
     // Placeholder output
-    mstStart.push_back(1);
-    mstEnd.push_back(2);
+
     cout << "TODO: Replace placeholder MST output with the actual MST.\n";
 
-    return 0; // TODO: Return the actual total cost of the MST
+    return totalCost; // TODO: Return the actual total cost of the MST
 }
